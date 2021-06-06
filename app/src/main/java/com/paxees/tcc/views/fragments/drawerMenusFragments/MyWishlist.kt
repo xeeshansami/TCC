2d0629617cfdfb08f06268e3871e8c33e8e563c0
xeeshan <info.xeeshan@gmail.com>
2021-06-06 18:49:37 +0500

    done

--- app/src/main/java/com/paxees/tcc/views/fragments/drawerMenusFragments/MyWishlist.kt
+++ app/src/main/java/com/paxees/tcc/views/fragments/drawerMenusFragments/MyWishlist.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.paxees.tcc.R
+import com.paxees.tcc.models.mFilterDashboard
 import com.paxees.tcc.utils.SessionManager
+import com.paxees.tcc.views.adapters.StrainAdapter
+import com.paxees.tcc.views.adapters.WishlistAdapter
+import kotlinx.android.synthetic.main.fragment_my_wishlist.*
 import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.toolbar.header
+import java.util.ArrayList
 
 class MyWishlist : Fragment(), View.OnClickListener {
     var sessionManager: SessionManager? = null
@@ -32,6 +39,7 @@ class MyWishlist : Fragment(), View.OnClickListener {
         sessionManager = SessionManager(activity)
         backBtn.setOnClickListener(this)
         header.text = getText(R.string.my_wishlist)
+        rvWishlist()
     }
 
     override fun onClick(v: View) {
@@ -47,5 +55,32 @@ class MyWishlist : Fragment(), View.OnClickListener {
         val graph = navController.graph
         graph.startDestination = startDestId
         navController.graph = graph
+    }
+    private fun rvWishlist() {
+        val rec: ArrayList<mFilterDashboard> = ArrayList<mFilterDashboard>()
+        val txt = ArrayList<String>()
+        val img = ArrayList<Int>()
+        txt.add("Alien Rift")
+        txt.add("Astro")
+        txt.add("Animal Face")
+        txt.add("Blue-Dream2")
+        txt.add("Actual Plants")
+        img.add(R.drawable.alien)
+        img.add(R.drawable.bruce_banner)
+        img.add(R.drawable.diagnose10img)
+        img.add(R.drawable.blue_dream2)
+        img.add(R.drawable.blueberry_og)
+        for (i in txt.indices) {
+            val filterDashboard = mFilterDashboard()
+            filterDashboard.setTxt(txt[i])
+            filterDashboard.img = img[i]
+            rec.add(filterDashboard)
+        }
+        // set up the RecyclerView
+        val horizontalLayoutManagaer = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        rvWishList!!.layoutManager = horizontalLayoutManagaer
+        var VideosAdapter = WishlistAdapter(activity, rec)
+        rvWishList!!.setAdapter(VideosAdapter)
+        VideosAdapter.notifyDataSetChanged()
     }
 }
