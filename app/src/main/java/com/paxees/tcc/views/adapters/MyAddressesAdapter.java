c90ffd14083489b7e094db9c5de866c67a6865c9
xeeshan <info.xeeshan@gmail.com>
2021-06-06 19:50:54 +0500

    done

---
+++ app/src/main/java/com/paxees/tcc/views/adapters/MyAddressesAdapter.java
@@ -0,0 +1,89 @@
+package com.paxees.tcc.views.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.paxees.tcc.R;
+import com.paxees.tcc.models.mFilterDashboard;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAddressesAdapter extends RecyclerView.Adapter<MyAddressesAdapter.ViewHolder> {
+    private int row_index;
+    private ArrayList<mFilterDashboard> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+    private Context context;
+    private String branchImage;
+
+    // data is passed into the constructor
+    public MyAddressesAdapter(Context context, ArrayList<mFilterDashboard> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        this.context = context;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_views_myaddresses, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        mFilterDashboard data = mData.get(position);
+        Glide.with(context).load(data.getImg()).into( holder.imageView);
+        holder.titleLbl.setText(data.getTxt());
+        holder.titleValue.setText(data.getValue());
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView titleLbl, titleValue;
+        ImageView imageView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            titleLbl = itemView.findViewById(R.id.txtCardNum);
+            titleValue = itemView.findViewById(R.id.visaCardTv);
+            imageView = itemView.findViewById(R.id.payment_method_done);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    mFilterDashboard getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/paxees/tcc/views/fragments/drawerMenusFragments/MyAddresses.kt
+++ app/src/main/java/com/paxees/tcc/views/fragments/drawerMenusFragments/MyAddresses.kt
@@ -6,9 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.paxees.tcc.R
+import com.paxees.tcc.models.mFilterDashboard
 import com.paxees.tcc.utils.SessionManager
+import com.paxees.tcc.views.adapters.MyAddressesAdapter
+import com.paxees.tcc.views.adapters.PaymentMethodAdapter
+import kotlinx.android.synthetic.main.fragment_add_payment_method.*
+import kotlinx.android.synthetic.main.fragment_add_payment_method.rvPayments
+import kotlinx.android.synthetic.main.fragment_myaddresses.*
 import kotlinx.android.synthetic.main.toolbar_theme.*
+import java.util.ArrayList
 
 class MyAddresses : Fragment(), View.OnClickListener {
     var sessionManager: SessionManager? = null
@@ -28,6 +36,7 @@ class MyAddresses : Fragment(), View.OnClickListener {
     fun init(view: View?) {
         sessionManager = SessionManager(activity)
         backBtn.setOnClickListener(this)
+        rvAddressFunc()
     }
 
     override fun onClick(v: View) {
@@ -36,5 +45,34 @@ class MyAddresses : Fragment(), View.OnClickListener {
                findNavController().popBackStack()
             }
         }
+    }
+
+    private fun rvAddressFunc() {
+        val rec: ArrayList<mFilterDashboard> = ArrayList<mFilterDashboard>()
+        val txt = ArrayList<String>()
+        val lbl = ArrayList<String>()
+        val img = ArrayList<Int>()
+        txt.add("Piata Unirii 2, Apartment23...")
+        txt.add("Piata Victoria 2, Apartment23...")
+        txt.add("Karachi Pakistan, Surjani...")
+        lbl.add("Home Address")
+        lbl.add("Work Address")
+        lbl.add("Office Address")
+        img.add(R.drawable.circle_img)
+        img.add(R.mipmap.ic_done)
+        img.add(R.drawable.circle_img)
+        for (i in txt.indices) {
+            val filterDashboard = mFilterDashboard()
+            filterDashboard.setTxt(txt[i])
+            filterDashboard.img = img[i]
+            filterDashboard.value = lbl[i]
+            rec.add(filterDashboard)
+        }
+        // set up the RecyclerView
+        val horizontalLayoutManagaer = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        rvAddresses.layoutManager = horizontalLayoutManagaer
+        var VideosAdapter = MyAddressesAdapter(activity, rec)
+        rvAddresses.setAdapter(VideosAdapter)
+        VideosAdapter.notifyDataSetChanged()
     }
 }
